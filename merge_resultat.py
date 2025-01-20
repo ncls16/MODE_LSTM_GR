1cf22b56e06000e764f6171c099d71996c330988
Loic <loic;lm82@gmail.com>
2025-01-20 11:36:52 +0100

    maj code merge

--- merge_resultat.py
+++ merge_resultat.py
@@ -1,29 +1,66 @@
 import pandas as pd
 import os
+import numpy as np
 
-dir_proj = os.path.normpath(os.getcwd()) #.replace('\\','/') # Répertoire actuel
+
+####################################################
+# --------------- Entrées (modififiable) -----------
+dir_proj = os.path.normpath(os.getcwd()) 
 dir_results = os.path.normpath(os.path.join(dir_proj, 'resultats')) # Répertoire des résultats
-name = ["Loic"] # Noms des fichiers #"Emma", "Loic", "Nicolas", "Fabio" 
+#name = ["Loic"] # Noms des fichiers #"Emma", "Loic", "Nicolas", "Fabio" 
 
+# fichier résultat GR4J
 file_GR4J = os.path.normpath(os.path.join(dir_results, 'resultats_GR4J.csv'))
 df_GR4J = pd.read_csv(file_GR4J)
 df_GR4J = df_GR4J.add_suffix('_GR4J')
 
+# statistiques par BV
 file_stat = os.path.normpath(os.path.join(dir_results, 'statistiques.csv'))
 df_stat = pd.read_csv(file_stat)
 
-# Création d'un DataFrame vide
-# best true or false
-df_resultats = pd.DataFrame()
-for file in name :
-    file_LSTM = os.path.normpath(os.path.join(dir_results, "resultats_LSTM_" + file + ".csv"))
-    df_LSTM = pd.read_csv(file_LSTM)
-    df_resultats = pd.concat([df_resultats, df_LSTM], ignore_index=True)
+#####################################################
 
-df_resultats = df_resultats.add_suffix('_LSTM')
-result = pd.merge(df_resultats, df_GR4J, how = 'left', left_on='BV_LSTM', right_on='BV_GR4J')   
-result = pd.merge(result, df_stat, how = 'left', left_on='BV_LSTM', right_on='BV')
-print(result)
+
+
+# récupération des fichiers de résultats
+resultats = os.listdir(dir_results)
+
+# Récupération données LSTM
+df_LSTM = pd.DataFrame()
+resultats = os.listdir(dir_results)
+print('fichiers résultats : ', resultats)
+for file in resultats :
+    if 'LSTM' in file:
+        file_path = os.path.join(dir_results, file)
+        df_temp = pd.read_csv(file_path)
+        df_LSTM = pd.concat([df_LSTM, df_temp], ignore_index=True)
+
+df_LSTM = df_LSTM.add_suffix('_LSTM')
+
+
+# # ----------------------Merge des données---------------------
+# merge LSTM + GR4J
+df_result = pd.merge(df_LSTM, df_GR4J, how = 'left', left_on='BV_LSTM', right_on='BV_GR4J')
+
+# merge avec les statistiques   
+df_result = pd.merge(df_result, df_stat, how = 'left', left_on='BV_LSTM', right_on='BV')
+
+print('df_result.shape : ',df_result.shape)
+
+
+# -----------------------Comparaison du temps de calcul---------------------
+noms = df_result['nom_LSTM'].unique()
+print('noms = ',noms)
+
+
+
+
+
+
+
+
+
+
 
 # BEST
 # code_BV = result['BV_LSTM'].unique()
